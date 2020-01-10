f034ee88f009a8d6dacc3a21daad8bce17edf702
yordi-a <yordi@outlook.com.tr>
2020-01-10 19:48:59 +0300

    Soda Makinesi Scripti

---
+++ locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+	['vending'] = 'Makine hazırlanıyor, lütfen bekle!',
+	['machine'] = 'Soda makinesi',
+	['machines'] = 'Soda makinesileri',
+	['press_context'] = 'Makineden 1$ ödeyerek soda almak istersen ~INPUT_CONTEXT~ tuşuna basabilirsin.',
+	['bought'] = 'Az önce 1$ ödeyerek bir şişe soda satın aldın!',
+	['not_enough'] = 'Yeterli kadar paran yok %s$ !',
+	['player_cannot_hold'] = 'Envanterinizde yeterli boş alan yok!',
+	['refreshment'] = 'Güzel bi ferhalık geldi!',
+	['broken'] = 'Bu makine çalışmıyor',
+	['out_of_product'] = 'Makine bu ürünün dışında',
+	['other_problem'] = 'Makine paranı yuttu ve sodanı vermedi',
+}
